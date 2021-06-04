fa50890cc692b317dbf78fa3edb78fc79bea7eb1
josephaw1022 <josephsims1@gmail.com>
2021-06-04 17:27:11 -0400

    new height and width features

--- frontend_testing/src/App.tsx
+++ frontend_testing/src/App.tsx
@@ -4,13 +4,13 @@ const App = () => {
   return (
     <div className=" fadeIn padding-px-0 margin-px-0 width-percent-100 height-percent-100">
       <div className="grid-template-row padding-percent-0">
-        <div className="  bg-white flex-row justify-space-between margin-percent-0 padding-percent-0 width-percent-100 flex-row justify-center align-items-center txt-center height-100-px">
+        <div className="  bg-white flex-row justify-space-between margin-percent-0 padding-percent-0 width-percent-100 flex-row justify-center align-items-center txt-center height-px-100">
           <h1 className="txt-black padding-left-px-5 h1-adjust margin-left-px-10 margin-right-px-10 word-break">Navbar</h1>
           <h1 className="txt-black padding-right-px-10 h1-adjust word-break">
             Joseph Whiteaker
           </h1>
         </div>
-        <div className=" flex-col pad-pixel-0 align-center justify-center txt-center height-100-percent">
+        <div className=" flex-col pad-pixel-0 align-center justify-center txt-center height-percent-100">
           <h1 className=" text-center txt-orange word-break"> Hello </h1>
           <div className=" button  align-self-center txt-center align-center bg-pink margin-pixel-2">
             <h1 className=" button-text fontpx40 txt-green">click me</h1>
--- python_scripts/padding.py
+++ python_scripts/padding.py
@@ -9,11 +9,11 @@ def format_class(name: str, attr: str):
     list_vals = []
     for i in range(0, 100):
         list_vals.append("."+name+str(i) +
-                         " { " + attr + ": " + str(i) + "px }")
+                         " { " + attr + ": " + str(i) + "% ; }")
     return list_vals
 
 
-x = format_class("top-px-", "top")
+x = format_class("height-px-", "height")
 
 file = open("output/output.txt", "w+")
 for i in range(len(x)):
